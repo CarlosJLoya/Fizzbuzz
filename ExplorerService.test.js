01027a80523d285bd2fb2d9fddcc73b0103e1ba4
Carlos Javier Loya Amaya <carlos.loya@Abitat.com.mx>
2022-04-28 18:39:41 -0600

    Actualizando  Ruta package.json para windows y agregando github Actions archivo yml

---
+++ ExplorerService.test.js
@@ -0,0 +1,20 @@
+
+const Reader = require("../lib/utils/Reader");
+const ExplorerService = require("../lib/services/ExplorerService");
+const explorers = Reader.readJsonFile("./test/explorerstest.json")
+describe("Unit test for Explorer Service class", () => {
+    test('1) Filtro por mision', () => {
+        expect(ExplorerService.filterByMission(explorers, "node").length).toBeGreaterThanOrEqual(0);
+
+    });
+    test('2) getAmountOfExplorersByMission function', () => {
+
+        expect(ExplorerService.getAmountOfExplorersByMission(explorers, "node").length).toBe(10);
+
+    })
+    test('3) getExplorersUsernamesByMission function', () => {
+
+        expect(ExplorerService.getExplorersUsernamesByMission(explorers, "node")).toContain("ajolonauta11");
+
+    })
+})
